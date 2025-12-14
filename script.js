fb66d3adf5260e78cc35988c2499dae460806722
djenidisimple <djenidigauss@gmail.com>
2025-12-14 17:15:23 +0300

    new feature adding

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let btn = document.querySelectorAll("button");
-let screen = document.querySelector(".screen");
+let screen = document.querySelector(".text-screen");
 let cal = 0,pre = 0, value = [];
-// screen.innerHTML = "399,981";
+let result = true;
 
 btn.forEach((button) => {
     button.addEventListener("click", function() {
@@ -9,11 +9,47 @@ btn.forEach((button) => {
             screen.innerHTML = screen.textContent.slice(0, -1);
         } else if (button.value == "=") {
             value = screen.textContent.match(/([-+]?\d+(?:\.\d+)?|[+\-*/])/g).join("");
-            screen.innerHTML = eval(value);
+            animateText(screen, eval(value));
+            result = true;
         } else if (button.value == "reset") {
-            screen.innerHTML = "";
+            animateText(screen, "");
         } else {
-            screen.innerHTML += button.value;
+            if (result) {
+                result = removeScreen(result, screen);
+                setTimeout(function(){
+                    screen.innerHTML += button.value;
+                }, 500);
+            } else {
+                screen.innerHTML += button.value;
+            }
         }
+        screen.innerText = deleteFormatedNumber(screen.textContent);
+        screen.innerText = formatedNumber(screen.textContent);
     });
-})
+});
+
+const removeScreen = (result, screen) => {
+    if (result) {
+        animateText(screen, "");
+        return !result;
+    }
+    return result;
+}
+
+const animateText = (screen, value) => {
+    screen.classList.add("animation-text-toGoUp");
+    setTimeout(function() {
+        screen.innerHTML = value;
+        screen.innerText = deleteFormatedNumber(screen.textContent);
+        screen.innerText = formatedNumber(screen.textContent);
+        screen.classList.remove("animation-text-toGoUp");
+    },500);
+}
+
+const formatedNumber = (value) => {
+    return value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+const deleteFormatedNumber = (value) => {
+    return value.split(",").join("");
+}
