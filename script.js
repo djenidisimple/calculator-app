604d1f06351d41ae1e2aff5b6284210ac83e948d
djenidisimple <djenidigauss@gmail.com>
2025-12-16 16:50:18 +0300

    i fixed mini bug

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let cal = 0,pre = 0, value = [];
 let result = true;
 let round = document.querySelectorAll(".round");
 let body = document.querySelector("body");
+let limite = document.querySelector(".limite");
 
 const savedTheme = localStorage.getItem('calculator-theme') || 1;
 round.forEach((r) => r.style.opacity = "0");
@@ -14,15 +15,19 @@ btn.forEach((button) => {
     button.addEventListener("click", function() {
         if (button.value == "del") {
             screen.innerHTML = screen.textContent.slice(0, -1);
+            if (screen.textContent.length == 0) {
+                screen.innerText = "0";
+                result = true;
+            }
         } else if (button.value == "=") {
             value = screen.textContent.match(/([-+]?\d+(?:\.\d+)?|[+\-*/])/g).join("");
-            animateText(screen, eval(value));
+            animateText(screen, limite, eval(value), true);
             result = true;
         } else if (button.value == "reset") {
-            animateText(screen, "");
+            animateText(screen, limite, "0");
         } else {
             if (result) {
-                result = removeScreen(result, screen);
+                result = removeScreen(result, limite, screen);
                 setTimeout(function(){
                     screen.innerHTML += button.value;
                 }, 500);
@@ -35,20 +40,25 @@ btn.forEach((button) => {
     });
 });
 
-const removeScreen = (result, screen) => {
+const removeScreen = (result, limite, screen) => {
     if (result) {
-        animateText(screen, "");
+        animateText(screen, limite, "");
         return !result;
     }
     return result;
 }
 
-const animateText = (screen, value) => {
+const animateText = (screen, limite, value, resultat=false) => {
     screen.classList.add("animation-text-toGoUp");
     setTimeout(function() {
         screen.innerHTML = value;
         screen.innerText = deleteFormatedNumber(screen.textContent);
         screen.innerText = formatedNumber(screen.textContent);
+        if ((limite.scrollWidth < screen.clientWidth) && resultat) {
+            limite.style.justifyContent = "flex-start";
+        } else {
+            limite.style.justifyContent = "flex-end";
+        }
         screen.classList.remove("animation-text-toGoUp");
     },500);
 }
